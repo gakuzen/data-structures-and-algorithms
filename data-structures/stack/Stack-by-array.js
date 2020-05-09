f3046f64e6448555accb21a13d52703f0b21f1ea
Leo Chan <chanwailok1994@gmail.com>
2020-05-09 14:14:52 +0800

    feat: stack

---
+++ data-structures/stack/Stack-by-array.js
@@ -0,0 +1,41 @@
+class Stack {
+  constructor() {
+    this.data = [];
+  }
+
+  push(item) {
+    this.data.push(item);
+  }
+
+  pop() {
+    if (this.data.length === 0) throw new Error("empty stack");
+    return this.data.pop();
+  }
+
+  peek() {
+    return this.data[this.data.length - 1];
+  }
+
+  isEmpty() {
+    return this.data.length === 0;
+  }
+
+  print() {
+    return this.data.reverse().join(" ");
+  }
+}
+
+const test = () => {
+  let s = new Stack();
+
+  s.push(1);
+  s.push(2);
+  s.push(3);
+  s.pop();
+
+  console.log(s.peek());
+  console.log(s.isEmpty());
+  console.log(s.print());
+};
+
+test();
---
+++ data-structures/stack/Stack-by-linked-list.js
@@ -0,0 +1,60 @@
+class Stack {
+  constructor() {
+    this.top;
+  }
+
+  push(item) {
+    const node = {
+      value: item,
+      next: null,
+    };
+
+    node.next = this.top;
+    this.top = node;
+  }
+
+  pop() {
+    if (!this.top) throw new Error("empty stack");
+
+    const top = this.top;
+    this.top = top.next;
+
+    return top.value;
+  }
+
+  peek() {
+    if (!this.top) throw new Error("empty stack");
+    return this.top.value;
+  }
+
+  isEmpty() {
+    return !this.top;
+  }
+
+  print() {
+    let array = [];
+
+    let node = this.top;
+    while (node) {
+      array.push(node.value);
+      node = node.next;
+    }
+
+    return array.join("->");
+  }
+}
+
+const test = () => {
+  let s = new Stack();
+
+  s.push(1);
+  s.push(2);
+  s.push(3);
+  s.pop();
+
+  console.log(s.peek());
+  console.log(s.isEmpty());
+  console.log(s.print());
+};
+
+test();
