8125c3199f72c8e20dd805c2e0f058f7ba7603f4
Leo Chan <chanwailok1994@gmail.com>
2020-05-10 11:57:08 +0800

    feat: queue

---
+++ data-structures/queue/Queue-by-array.js
@@ -0,0 +1,41 @@
+class Queue {
+  constructor() {
+    this.data = [];
+  }
+
+  enqueue(item) {
+    this.data.push(item);
+  }
+
+  dequeue() {
+    return this.data.shift();
+  }
+
+  peek() {
+    return this.data[0];
+  }
+
+  isEmpty() {
+    return !this.data;
+  }
+
+  print() {
+    return this.data;
+  }
+}
+
+const test = () => {
+  const queue = new Queue();
+
+  queue.enqueue(1);
+  queue.enqueue(2);
+  queue.enqueue(3);
+  queue.enqueue(4);
+  queue.enqueue(5);
+
+  console.log(queue.dequeue());
+  console.log(queue.peek());
+  console.log(queue.print());
+};
+
+test();
---
+++ data-structures/queue/Queue-by-linked-list.js
@@ -0,0 +1,66 @@
+class Queue {
+  constructor() {
+    this.front = null;
+    this.end = null;
+  }
+
+  enqueue(item) {
+    const node = {
+      value: item,
+      next: null,
+    };
+
+    if (!this.front) this.front = node;
+    if (this.end) this.end.next = node;
+    this.end = node;
+  }
+
+  dequeue() {
+    if (!this.front) throw new Error("empty queue");
+
+    const front = this.front;
+    this.front = front.next;
+    if (!this.front) {
+      this.end = null;
+    }
+
+    return front.value;
+  }
+
+  peek() {
+    if (!this.front) throw new Error("empty queue");
+    return this.front.value;
+  }
+
+  isEmpty() {
+    return !this.front;
+  }
+
+  print() {
+    const array = [];
+    let node = this.front;
+
+    while (node) {
+      array.push(node.value);
+      node = node.next;
+    }
+
+    return array.join("->");
+  }
+}
+
+const test = () => {
+  const queue = new Queue();
+
+  queue.enqueue(1);
+  queue.enqueue(2);
+  queue.enqueue(3);
+  queue.enqueue(4);
+  queue.enqueue(5);
+
+  console.log(queue.dequeue());
+  console.log(queue.peek());
+  console.log(queue.print());
+};
+
+test();
